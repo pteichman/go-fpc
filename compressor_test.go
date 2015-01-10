d15c2fea286089f8fce5c009a74c576d4ab95fc9
Peter Teichman <peter@teichman.org>
2015-01-09 16:34:20 -0800

    Fill out TestRoundtripWithTwoValues a little

--- compressor_test.go
+++ compressor_test.go
@@ -1,7 +1,7 @@
 package fpc
 
 import (
-	"log"
+	"math"
 	"testing"
 )
 
@@ -10,7 +10,7 @@ func TestLongToByteArray(t *testing.T) {
 
 	for i := 0; i < 10000; i++ {
 		if comp.ToLong(comp.ToByteArray(int64(i))) != int64(i) {
-			t.Fatalf("Bad round trip: %d", i)
+			t.Fatalf("Failed value round trip: %d", i)
 		}
 	}
 }
@@ -20,12 +20,33 @@ func TestRoundtripWithTwoValues(t *testing.T) {
 		Vals []float64
 	}{
 		{[]float64{1.0, 0.0}},
+		{[]float64{3.0, 0.0}},
 	}
-
-	comp := NewCompressor(16)
 
 	for _, tt := range tests {
+		comp := NewCompressor(16)
 		vals := comp.Decompress(comp.Compress(tt.Vals))
-		log.Println(vals)
+
+		if !almostEqualSlice(tt.Vals, vals) {
+			t.Fatalf("Failed round trip: %v (got %v)", tt.Vals, vals)
+		}
 	}
+}
+
+func almostEqualSlice(v1, v2 []float64) bool {
+	if len(v1) != len(v2) {
+		return false
+	}
+
+	for i := 0; i < len(v1); i++ {
+		if !almostEqual(v1[i], v2[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func almostEqual(v1, v2 float64) bool {
+	return math.Abs(v1-v2) < 0.00000001
 }
